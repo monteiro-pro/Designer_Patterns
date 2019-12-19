34bcddcb575d0f82e5f6dfc9cc0dd239a181219e
Jose Monteiro da Silva <j.monteiro.da.silva@avanade.com>
2019-12-18 23:38:03 -0300

    Comportamental - Interpreter

---
+++ Interpreter/CentenasExpressao.cs
@@ -0,0 +1,30 @@
+﻿namespace Interpreter
+{
+    public class CentenasExpressao : Expressao
+    {
+        public override string Cinco()
+        {
+            return "D";
+        }
+
+        public override int Multiplicar()
+        {
+            return 100;
+        }
+
+        public override string Nove()
+        {
+            return "CM";
+        }
+
+        public override string Quatro()
+        {
+            return "CD";
+        }
+
+        public override string Um()
+        {
+            return "C";
+        }
+    }
+}
---
+++ Interpreter/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string romano = "MCMXXVIII";
+            Contexto contexto = new Contexto(romano);
+
+            List<Expressao> lista = new List<Expressao>();
+            lista.Add(new MilharesExpressao());
+            lista.Add(new CentenasExpressao());
+            lista.Add(new DezenasExpressao());
+            lista.Add(new UnidadeExpressao());
+
+            foreach (Expressao exp in lista)
+            {
+                exp.Interpreter(contexto);
+            }
+
+            Console.WriteLine("{0} - {1}", romano, contexto.Output);
+
+            Console.ReadKey();
+        }
+    }
+}
