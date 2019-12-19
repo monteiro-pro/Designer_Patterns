2de155c19a17387e7d57e149031fb6dee797dcea
Jose Monteiro da Silva <j.monteiro.da.silva@avanade.com>
2019-12-18 21:30:37 -0300

    Comportamental - Command

---
+++ Command/Command.cs
@@ -0,0 +1,14 @@
+﻿namespace Command
+{
+    public abstract class Command
+    {
+        protected Receiver Receiver;
+
+        public Command(Receiver receiver)
+        {
+            this.Receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+}
