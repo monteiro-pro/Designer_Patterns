0ccd471478d91bc9b43a4787554528921c8805d2
Jose Monteiro da Silva <j.monteiro.da.silva@avanade.com>
2019-12-20 10:23:41 -0300

    Comportamental - Memento

---
+++ Memento/Memento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Memento
+{
+    public class Memento
+    {
+        private string _state;
+
+        public Memento(string state)
+        {
+            this._state = state;
+        }
+
+        public string State
+        {
+            get { return this._state; }
+        }
+    }
+}
---
+++ Memento/Originator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Memento
+{
+    public class Originator
+    {
+        private string _state;
+
+        public string State
+        {
+            get {return this._state; }
+            set 
+            {
+                _state = value;
+                Console.WriteLine("State = " + _state);
+            }
+        }
+
+        public Memento CreateMementor()
+        {
+            return (new Memento(this._state));
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            Console.WriteLine("Restaurando o estado...");
+            State = memento.State;
+        }
+    }
+}
