7a6ba9b91797546212dad25592f050e6ff0b5392
Jose Monteiro da Silva <j.monteiro.da.silva@avanade.com>
2019-12-23 16:16:06 -0300

    Comportamental - Template Method

---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass abstrata_a = new ConcreteClassA();
+            abstrata_a.TemplateMethod();
+
+            AbstractClass abstrata_b = new ConcreteClassB();
+            abstrata_b.TemplateMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
